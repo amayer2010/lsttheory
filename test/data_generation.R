9585fc1fb13354fc91d95bd0b15628c62105e725
Axel Mayer <amayer2010@gmail.com>
2013-12-25 01:28:44 +0100

    datasets added for AR article

--- test/data_generation.R
+++ test/data_generation.R
@@ -1,7 +1,46 @@
 
 library(MASS)
 
-## dataset: multistate model
+########### dataset: CTT model for software appendix ###########
+
+set.seed(222999)
+
+N <- 300
+eta <- rnorm(N,2,2)
+y1 <- eta + rnorm(N,0,0.7)
+y2 <- 0.5 + 0.8*eta + rnorm(N,0,0.6)
+y3 <- 0.9 + 1.1*eta + rnorm(N,0,0.7)
+
+taucong <- data.frame(y1,y2,y3)
+
+save(taucong, file="../data/taucong.RData")
+
+
+################## dataset: multistate model for software appendix ###################
+
+set.seed(8882211)
+
+N <- 400
+sigma <- matrix(c(3,2,2,3),nrow=2,byrow=T)
+mu <- c(0.5,1)
+
+d <- mvrnorm(N,mu,sigma)
+
+eta1 <- d[,1]
+eta2 <- d[,2]
+
+y11 <- eta1 + rnorm(N,0,0.5)
+y21 <- 0.5 + 0.8*eta1 + rnorm(N,0,0.5)
+
+y12 <- eta2 + rnorm(N,0,0.6)
+y22 <- 0.7 + 0.9*eta2 + rnorm(N,0,0.5)
+
+multistate <- data.frame(y11,y21,y12,y22)
+save(multistate, file="../data/multistate.RData")
+
+
+
+################## dataset: multistate model (older version with 3 time points) ###################
 
 set.seed(3423532)
 
@@ -27,13 +66,13 @@ y13 <- 0 + 1*eta3 + rnorm(N)
 y23 <- 0.3 + 1.2*eta3 + rnorm(N)
 y33 <- 0.6 + 0.8*eta3 + rnorm(N)
 
-multistate <- data.frame(y11,y21,y31,y12,y22,y32,y13,y23,y33)
+multistate02 <- data.frame(y11,y21,y31,y12,y22,y32,y13,y23,y33)
 
-save(multistate, file="data/multistate.RData")
+save(multistate02, file="../data/multistate02.RData")
 
 
 
-## dataset: multitrait-multistate model
+################ dataset: multitrait-multistate model ###################
 
 set.seed(8454312)
 
@@ -63,7 +102,11 @@ y23 <- 0.3 + 1.2*eta3 + rnorm(N)
 y14 <- 0 + 1*eta4 + rnorm(N)
 y24 <- 0.3 + 1.2*eta4 + rnorm(N)
 
-multitraitmultistate <- data.frame(y11,y21,y12,y22,y13,y23,y14,y24)
+x <- rbinom(N,1,plogis(xi2, location=1))
 
-save(multitraitmultistate, file="data/multitraitmultistate.RData")
+multitraitmultistate <- data.frame(y11,y21,y12,y22,y13,y23,y14,y24)
+multitraitmultistate02 <- data.frame(y11,y21,y12,y22,y13,y23,y14,y24,x)
+
+save(multitraitmultistate, file="../data/multitraitmultistate.RData")
+save(multitraitmultistate02, file="../data/multitraitmultistate02.RData")
 
