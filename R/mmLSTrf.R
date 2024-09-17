99994306bec28a6ca3549de716afdd094d42eb67
DoraTinhof <dora.tinhof@uni-bielefeld.de>
2024-09-17 17:24:19 +0200

    Updated mmLSTrf() parameter notation information

--- R/mmLSTrf.R
+++ R/mmLSTrf.R
@@ -28,6 +28,7 @@
 #'
 #'\strong{Syntax notation:}\cr
 #'Example for Y_2132 with i = 2, m = 1, t = 3, s = 2 (covariances with s = 3)
+#'\tabular{lll}{
 #' \strong{Variable / Parameter}           \tab \strong{Notation}  \tab \strong{Example}   \cr
 #' Trait factors (TF)                      \tab Ts                 \tab T2                 \cr
 #' Occasion factors (OF)                   \tab Ots                \tab O32                \cr
